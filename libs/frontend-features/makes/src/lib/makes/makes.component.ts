2efe0f39f10cc1cd5132262af71382e4ca896792
Victor Velazquez Cid <victorvelazquezcid@gmail.com>
2023-08-08 15:37:30 -0400

    Add shared lib

--- libs/frontend-features/makes/src/lib/makes/makes.component.ts
+++ libs/frontend-features/makes/src/lib/makes/makes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiModule } from '@nx-carstore-monorepo/shared/ui';
 
 @Component({
   selector: 'nx-carstore-monorepo-makes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UiModule],
   templateUrl: './makes.component.html',
   styleUrls: ['./makes.component.css'],
 })
