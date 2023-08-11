4fae7c58d0749af703ff8bd82c5097aa2ce933e5
Victor Velazquez Cid <victorvelazquezcid@gmail.com>
2023-08-11 11:46:44 -0400

    Add interceptor to api

--- apps/carstore-api/src/main.ts
+++ apps/carstore-api/src/main.ts
@@ -7,9 +7,11 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { ApiresponseInterceptor } from '@nx-carstore-monorepo/backend-features/makes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ApiresponseInterceptor());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
